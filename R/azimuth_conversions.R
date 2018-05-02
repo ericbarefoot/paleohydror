8d54b1a34324fa1e6753faa29e96b0a2bfe35ece
Eric Barefoot <eabarefoot@gmail.com>
2018-05-01 23:09:57 -0400

    updating documentation. file organization

--- R/azimuth_conversions.R
+++ R/azimuth_conversions.R
@@ -1,60 +1,12 @@
-#	A set of functions to convert 'quadrant' based directional measurements into more easily manipulated 'azimuth' based measurements. Beware, some are more easy to use than others. I recommend 'azStringConvert' since it is the only one with documentation.
+#	A function to convert 'quadrant' based directional measurements into more easily manipulated 'azimuth' based measurements.
 #	Eric Barefoot
 #	Nov 2017
-
-azconvert = function(datalist) {
-	# converts all measurements to hand-corrected azimuths with dip
-	# type 1 = strike to the NW and dip to the right
-	# type 2 = strike to the NW and dip to the left
-	# type 3 = strike to the NE and dip to the right
-	# type 4 = strike to the NE and dip to the left
-	# datalist is of the form [[1:4]]	
-	strike = datalist[[1]]
-	dip = datalist[[2]]
-	type = datalist[[3]]
-	for (i in 1:length(strike)) {
-		if (type[i] == 1) {
-			strike[i] = 360 - strike[i]
-		} else if(type[i] == 2) {
-			strike[i] = 180 - strike[i]
-		} else if (type[i] == 3) {
-			strike[i] = strike[i]
-		} else if (type[i] == 4) {
-			strike[i] = 180 + strike[i]
-		} else {
-			stop(paste('You forgot to denote type for datapoint', i, '.', sep = ' '))
-		}
-	}
-	out = cbind(strike, dip)
-	return(out)
-}
-    
-lineconvert = function(data) {
-    trend = data[[1]]
-    plunge = data[[2]]
-    type = data[[3]]
-    for (i in 1:length(trend)) {
-        if(type[i] == 1) {
-            trend[i] = 360 - trend[i]
-        } else if (type[i] == 2) {
-            trend[i] = trend[i]
-        } else if (type[i] == 3) {
-            trend[i] = 180 + trend[i]
-        } else if (type[i] == 4) {
-            trend[i] = 180 - trend[i]
-        } else {
-            stop('you fukked up')
-        }
-    }
-	out = cbind(trend, plunge)
-	return(out)
-}
 
 #' Azimuth conversion for 'quadrant' based strike/trend measurements
 #'
 #' Takes measurements of strike or trend like N45E and converts it to an azimuth measurement with N = 0 and E = 90.
 #'
-#' @param string	A string measurement with format [N/S][##][E/W]. Can be either X00X or X0X. Make sure you're using the right hand rule to determine how quadrant measurements are made, otherwise you're screwed. 
+#' @param string	A string measurement with format [N/S][##][E/W]. Can be either X00X or X0X. Make sure you're using the right hand rule to determine how quadrant measurements are made, otherwise you're screwed.
 #' @return A numeric azimuth trend.
 #' @export
 
--- R/paleohydror.r
+++ R/paleohydror.r
@@ -1,33 +1,39 @@
 #' paleohydror: A package for implementing published routines in paleohydrologic reconstruction.
 #'
-#' This package offers several functions and routines for deriving estimates of paleohydrology from field observations of rock outcrops. Data such as grain size are important too. 
-#' 
+#' This package offers several functions and routines for deriving estimates of paleohydrology from field observations of rock outcrops. Data such as grain size are important too.
+#'
 #' @section paleohydror functions:
-#' 
-#' The functions here make up a mix of tools to calculate and estimate modern suficial processes and back-calculate those processes in ancient rivers. 
-#' 
+#'
+#' The functions here make up a mix of tools to calculate and estimate modern suficial processes and back-calculate those processes in ancient rivers.
+#'
 #' \code{settle} calculates the settling velocity of particles following the formulation by Dietrich (1982).
-#' 
+#'
 #' \code{lynds_one} estimates ancient river slope using the first method described by Lynds et al. (2014)
-#' 
+#'
 #' \code{lynds_two} estimates ancient river slope using the second method described by Lynds et al. (2014)
-#' 
+#'
 #' \code{lynds_three} estimates ancient river slope using the third method described by Lynds et al. (2014)
 #'
 #' \code{xset2H} estimates ancient river dune heights based on cross-set thicknesses. Methods from Leclair and Paola.
-#' 
+#'
 #' \code{H2h} estimates ancient river depths based on dune heights. Methods from Bradley and Venditti.
-#' 
+#'
 #' \code{xset2depthSimple} estimates ancient river depths directly from cross-set thicknesses using the simplest assumptions. Methods from Leclair, Paola, and Bradley and Venditti.
 #'
 #' \code{azStringConvert} Azimuth conversion for 'quadrant' based strike/trend measurements.
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
+#'
+#' \code{ancient_1} Estimating sediment flux in ancient rivers following Mahon + McElroy (2018).
+#'
+#' \code{modern_1} Estimating sediment flux in modern rivers if bedform height is known following Mahon + McElroy (2018).
+#'
+#' \code{modern_2} Estimating sediment flux in modern rivers if bedform length is known following Mahon + McElroy (2018).
+#'
+#' \code{bedform_velocity} Estimating bedform migration rate as a function of reach-averaged slope.
+#'
+#' \code{bedform_bedload} Estimating bedload as a function of bedform height and migration rate. After Simons et al. (1965)
+#'
+#' \code{height_length} Estimating bedform height if bedform length is known.
+#'
 #' @docType package
 #' @name paleohydror
 NULL
